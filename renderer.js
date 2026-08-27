1a9d73309da7cac9517c96772634fca9aaa80a84
kool-kitty343 <1011mikebaker@gmail.com>
2026-08-27 16:41:40 -0400

    Initialize canvas and context in renderer.js

--- renderer.js
+++ renderer.js
@@ -1,1 +1,6 @@
-
+const canvas = document.createElement('canvas');
+canvas.width = 1600;
+canvas.height = 900;
+const ctx = canvas.getContext('2d');
+const imgData = ctx.createImageData(1600,900);
+const buffer = imgData.data;
