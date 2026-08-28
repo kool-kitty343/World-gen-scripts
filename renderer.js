a288074604b457a22ab9b6cc67b6e28bc0d16c91
kool-kitty343 <1011mikebaker@gmail.com>
2026-08-28 02:15:28 -0400

    Refactor ocean height calculation for clarity

--- renderer.js
+++ renderer.js
@@ -43,7 +43,7 @@ for(let index = 0; index < 1600 * 900; index++)
   }
   else if(110 >= heightMap[index]) // ocean
   {
-    buffer[pixel + 1] = Math.max(0, heightMap[index] - 55) * 2;
+    buffer[pixel + 1] = (heightMap[index] > 55) ? (heightMap[index] - 55) * 2 : 0;
     buffer[pixel + 2] = 255;
   }
 }
