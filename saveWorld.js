3ceb7dec75188722242c918eef2b04df041fd1b0
kool-kitty343 <1011mikebaker@gmail.com>
2026-08-29 13:03:37 -0400

    1

--- saveWorld.js
+++ saveWorld.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const saveName = SAVENAME;
-fs.mkdirSync('saves/' + saveName, {recursive:true});
-fs.writeFileSync('saves/' + saveName + '/noise.txt', "document.Noise");
+//fs.mkdirSync('saves/' + saveName, {recursive:true});
+fs.writeFile('saves/' + saveName + '/noise.txt', "document.Noise");
