bb74040e01e9a7fee035f5c94811b3b50605e745
kool-kitty343 <1011mikebaker@gmail.com>
2026-08-29 13:27:26 -0400

    1

--- saveWorld.js
+++ saveWorld.js
@@ -3,7 +3,7 @@ const saveName = SAVENAME;
 async function save(map)
 {
     await fs.mkdir('saves/' + saveName, {recursive:true});
-    await fs.writeFile('saves/' + saveName + '/' + map + '.txt', document[map]);
+    await fs.writeFile(`saves/${saveName}/${map}.txt`, document[map]);
 }
 for(const map of document.mapList)
 {
