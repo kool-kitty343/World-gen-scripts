1b529fc4d289b0660f93013902d9c9f59ee72aac
kool-kitty343 <1011mikebaker@gmail.com>
2026-08-29 13:58:53 -0400

    test

--- saveWorld.js
+++ saveWorld.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs/promises');
+const maps = document.mapList;
 const saveName = SAVENAME;
-await fs.mkdir('saves/' + saveName, {recursive:true});
+//await fs.mkdir('saves/' + saveName, {recursive:true});
 async function saveRaw(map)
 {
     await fs.writeFile(`saves/${saveName}/${map}.txt`, document[map]);
@@ -10,7 +11,7 @@ async function savePNG(map)
     await fs.writeFile(`saves/${saveName}/${map}.png`, document[`${map}PNG`]);
 }
 await save("Noise");
-for(const map of document.mapList)
+for(const map of maps)
 {
     await saveRaw(map);
     await savePNG(map);
