6b78a72adeb824a1d844b9987c562f4d744e0783
kool-kitty343 <1011mikebaker@gmail.com>
2026-08-29 13:17:53 -0400

    1

--- saveWorld.js
+++ saveWorld.js
@@ -2,7 +2,7 @@ const fs = require('node:fs/promises');
 //const saveName = SAVENAME;
 async function save()
 {
-    await fs.mkdir('saves/' + saveName, {recursive:true});
+    await fs.mkdir('saves/' + 'saveName', {recursive:true});
     await fs.writeFile('saves/' + 'saveName' + '/noise.txt', "document.Noise");
 }
 save();
